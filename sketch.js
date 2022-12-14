e103340af913d914a111ccda88c754c518d65fcc
viniciustdaik <viniciuskriiger2008@gmail.com>
2022-12-14 15:00:14 -0300

    update

--- sketch.js
+++ sketch.js
@@ -4,35 +4,43 @@ function setup() {
     createCanvas(windowWidth, windowHeight);
 
     emailInput = createInput("viniciuskriiger2008@gmail.com").attribute("placeholder", "Email");
-    emailInput.position(width / 2 - 65, height / 2 - 90);
+    emailInput.position(width / 2 - 85, height / 2 - 90);
     emailInput.size(200, 30);
 
     passwordInput = createInput("").attribute("placeholder", "Senha");
-    passwordInput.position(width / 2 - 65, height / 2 - 50);
+    passwordInput.position(width / 2 - 85, height / 2 - 50);
     passwordInput.size(200, 30);
 
     signInButton = createButton("Entrar");
-    signInButton.position(width / 2 - 35, height / 2);
+    signInButton.position(width / 2 - 55, height / 2);
     signInButton.size(150, 50);
+    signInButton.style("background-color:blue");
+    signInButton.style("font-size:45px");
+    signInButton.style("border-radius:25px");
     signInButton.mousePressed(() => this.signIn());
 
     signUpButton = createButton("Cadastrar");
-    signUpButton.position(width / 2 - 35, height / 2 + 52.5);
-    signUpButton.size(150, 50);
+    signUpButton.position(width / 2 - 65, height / 2 + 52.5);
+    signUpButton.size(170, 50);
+    signUpButton.style("background-color:lightblue");
+    signUpButton.style("font-size:35px");
+    signUpButton.style("border-radius:25px");
     signUpButton.mousePressed(() => this.signUp());
 
     signOutButton = createButton("Sair");
-    signOutButton.position(width / 2 - 35, height / 2 + 104.5);
+    signOutButton.position(width / 2 - 55, height / 2); //height / 2 + 104.5
     signOutButton.size(150, 50);
     signOutButton.style("background-color:red");
     signOutButton.style("font-size:45px");
+    signOutButton.style("border-radius:25px");
     signOutButton.hide();
     signOutButton.mousePressed(() => this.signOut());
 
     deleteButton = createButton("Deletar");
-    deleteButton.position(width / 2 - 45, height / 2 + 184.5);
+    deleteButton.position(width / 2 - 65, height / 2 + 52.5); //height / 2 + 184.5
     deleteButton.style("background-color:red");
     deleteButton.style("font-size:45px");
+    deleteButton.style("border-radius:25px");
     deleteButton.size(170, 50);
     deleteButton.hide();
     deleteButton.mousePressed(() => this.Delete());
@@ -48,8 +56,19 @@ function draw() {
         //text("uid: " + firebase.auth().currentUser.uid, width / 2, 35);
         text("" + firebase.auth().currentUser.email, width / 2, 35);
 
+        signInButton.hide();
+        signUpButton.hide();
+        emailInput.hide();
+        passwordInput.hide();
         signOutButton.show();
         deleteButton.show();
+    } else {
+        signInButton.show();
+        signUpButton.show();
+        emailInput.show();
+        passwordInput.show();
+        signOutButton.hide();
+        deleteButton.hide();
     }
 
     drawSprites();
